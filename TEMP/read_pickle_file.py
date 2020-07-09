22ec6cc2d8e078c85178e28ac5e3ebb87a45951d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-07-09 11:17:55 +0500

    updated

--- TEMP/read_pickle_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-import pickle
-file_name = sys.argv[1]
-a = pickle.load(open(file_name, "rb"))
-print(a)
-a.to_csv(file_name + ".csv", index=False)
-print("cpied to" + file_name + ".csv")
--- TEMP/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#exit
-
-del -rf /data/old       ; mkdir /data/old
-del -rf /data/dbpush    ; mkdir /data/dbpush 
-del -rf /data/History   ; mkdir /data/History
-del -rf logs/*
-del -rf /data/temporary ; mkdir /data/temporary
-./server23_amir_version_etl_process.py 1
